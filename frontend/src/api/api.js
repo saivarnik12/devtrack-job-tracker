d689c7676498c353c5296ed1a1cf597074c92e0d
Kammari Sai Varnik <56470235+saivarnik12@users.noreply.github.com>
2026-02-09 11:34:34 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,3 @@
-import axios from 'axios';const API=axios.create({baseURL:'http://localhost:5000/api'});
+import axios from 'axios';const API=axios.create({baseURL:baseURL: "https://devtrack-api-b2f8.onrender.com/api"});
 API.interceptors.request.use(req=>{const t=localStorage.getItem('token');if(t)req.headers.Authorization=`Bearer ${t}`;return req;});
 export default API;
