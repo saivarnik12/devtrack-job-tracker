9aa98dc787b256adc63d3051a876fc21c37723c6
Kammari Sai Varnik <56470235+saivarnik12@users.noreply.github.com>
2026-02-09 11:54:11 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,20 @@
-import axios from 'axios';const API=axios.create({baseURL:baseURL: "https://devtrack-api-b2f8.onrender.com/api"});
-API.interceptors.request.use(req=>{const t=localStorage.getItem('token');if(t)req.headers.Authorization=`Bearer ${t}`;return req;});
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://devtrack-api-b2f8.onrender.com/api",
+});
+
+API.interceptors.request.use((req) => {
+  const profile = localStorage.getItem("profile");
+
+  if (profile) {
+    const token = JSON.parse(profile).token;
+    if (token) {
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return req;
+});
+
 export default API;
